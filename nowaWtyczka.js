4d37ab657fa941b440abc9ce774b352e85aad6dc
alicjablazejewska1989 <allicespring21@gmail.com>
2016-10-01 21:03:23 +0200

    pliki z html
    przejrze

---
+++ nowaWtyczka.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ablazejewska on 01.10.16.
+ */
+
+(function ($) {
+    $.fn.cosik= function(){
+        $(this).parent().parent().remove();
+    }
+})(jQuery);
+
---
+++ nowySkrypt.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ablazejewska on 01.10.16.
+ */
+
+
+$("button").on("click", function(){
+    $(this).cosik();
+
+});
